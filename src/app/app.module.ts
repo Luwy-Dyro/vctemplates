8dc10dd68fec9a7331f840bf9850b2b85b8a0f92
Luwy.Dyro <luwy.dyro@gmail.com>
2019-10-08 10:58:44 -0500

    Upgrade in Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,42 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register/register.component';
+
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
 
+//Modulos
+import { PagesModule } from './pages/pages.module';
 
 
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     APP_ROUTES,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
