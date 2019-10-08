4073fadf5e42634a67b0b97b8729faca68cda599
Luwy.Dyro <luwy.dyro@gmail.com>
2019-10-07 20:55:32 -0500

    Inicio deñ project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
+
+
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
+    APP_ROUTES,
     BrowserModule,
     AppRoutingModule
   ],
